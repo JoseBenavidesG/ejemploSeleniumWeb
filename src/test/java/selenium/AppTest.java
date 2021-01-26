10cf8ec929d25a9c9d23057be116cff1009d9ad9
joseBenavidesg <josebenavidesg@gmail.com>
2021-01-25 22:08:33 -0300

    Se agrega la busqueda y compra de libro en amazon

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -21,7 +22,7 @@ public class AppTest
     @Before
     public void setUp(){
         System.out.println("Iniciando configuración...");
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://www.amazon.com");
         driver.manage().window().maximize();
@@ -35,8 +36,37 @@ public class AppTest
     {
         System.out.println("Iniciando Pruebas...");
         WebElement searchbox = driver.findElement(By.name("q"));
-        searchbox.sendKeys("HandBook Devops");
+        searchbox.sendKeys("Libros de Devops");
         searchbox.submit();
-        assertEquals("HandBook Devops", driver.getTitle());
+        driver.findElement(By.cssSelector(".ct5Ked:nth-child(3) .keP9hb")).click();
+        {
+          WebElement element = driver.findElement(By.cssSelector(".s1SShd .keP9hb"));
+          Actions builder = new Actions(driver);
+          builder.moveToElement(element).perform();
+        }
+        {
+          WebElement element = driver.findElement(By.tagName("body"));
+          Actions builder = new Actions(driver);
+          builder.moveToElement(element, 0, 0).perform();
+        }
+        driver.findElement(By.cssSelector(".qrShPb > span")).click();
+        driver.findElement(By.cssSelector(".tF2Cxc:nth-child(2) .LC20lb > span")).click();
+        {
+          WebElement element = driver.findElement(By.id("add-to-cart-button-ubb"));
+          Actions builder = new Actions(driver);
+          builder.moveToElement(element).perform();
+        }
+        driver.findElement(By.id("add-to-cart-button-ubb")).click();
+        {
+          WebElement element = driver.findElement(By.tagName("body"));
+          Actions builder = new Actions(driver);
+          builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+          WebElement element = driver.findElement(By.id("nav-link-accountList"));
+          Actions builder = new Actions(driver);
+          builder.moveToElement(element).perform();
+        }
+      
     }
 }
